4f838f05b29763204b0d3ca4d8f5422a69502c69
Camden_MacDowell <macdowell.camden@gmail.com>
2019-11-26 12:23:14 -0500

    commit

--- Spock/Spock_ClusterBehavior.m
+++ Spock/Spock_ClusterBehavior.m
@@ -1,5 +1,6 @@
 function Spock_ClusterBehavior(block,k)
 
+addpath(getpath('/jukebox/buschman/Rodent Data/Wide Field Microscopy/Widefield_Imaging_Analysis'));
 load('data_pheno');
 [ovr_comm, ovr_Q] = PhenographSimple(features_downsampled, 'knn', k);
 save(sprintf('block%d',block'),'ovr_comm','ovr_Q');
