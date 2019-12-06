3a755ac866c50563e0978e40e8f592959aeb4d6b
Camden_MacDowell <macdowell.camden@gmail.com>
2019-12-06 18:14:16 -0500

    update

--- Plotting/fig_params.m
+++ Plotting/fig_params.m
@@ -15,6 +15,9 @@ classdef fig_params
         c_discovery = [0.3 0.7 1]; %light blue
         c_pca = [0.105,0.319,0.46] %dark green
         c_nmf = [0.850,0.3723,0.00784]; %orange
+        c_wh_same = [0 0 0.75]; %withheld, same animal
+        c_wh_diff = [0.3 0.7 .4]; %withheld, different animal
+        c_static = [0.5 0.5 0.5]; %static 
         c_sensory = [0.3 0.7 0; 0.3 0.7 1; 0.7,0.1,0] %aud,air,vis. 
         c_visual = [0, 0, 1; 0.7,0.1,0 ];
         c_tactile = [1, 0, 0; 0.3, 0.7, 1];
